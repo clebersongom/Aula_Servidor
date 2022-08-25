ee62e02fe174b537d1b29090e004933bee2aef4b
clebersongom <clebersongom@gmail.com>
2022-08-24 21:03:13 -0300

    criando paginas

--- appConsulta/views.py
+++ appConsulta/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from appConsulta.models import DadosSecador
 
 def index(request):
-    dadosSecador = DadosSecador.objects.all()   
-    return render(request, 'consulta/index.html', {'dadosSecador' : dadosSecador})
+    dadosSecador = DadosSecador.objects.all() 
+    teste = [50,25]
+    
+    
+    return render(request, 'consulta/index.html', {'dadosSecador' : dadosSecador, 'teste': teste})
   
