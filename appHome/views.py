06715f81b52811f4a34de029e42ec486ca9cf9d3
clebersongom <clebersongom@gmail.com>
2022-09-11 06:24:19 -0300

    melhorias na Home + consultas no Banco

--- appHome/views.py
+++ appHome/views.py
@@ -1,10 +1,18 @@
 from queue import Empty
+from sqlite3 import Timestamp
 from django.shortcuts import render
 from appCadastra.models import CadastroOp
 from django.db.models import Q
 
 def index(request):
+    if request.method == 'POST':
+        
+        abrir_op = request.POST['abrir_op']
+        atualiza =CadastroOp.objects.filter(numero_op = abrir_op).update(hora_inicio_prod = '2020')
+        op_aberta = CadastroOp.objects.filter(numero_op = abrir_op)
+    else:
+        op_aberta = ''   
 
     opsCadastradas = CadastroOp.objects.filter(hora_inicio_prod = '') # busca se diferente de ''
     opsIniciadas = CadastroOp.objects.filter(~Q(hora_inicio_prod = ''))
-    return render(request, 'home/index.html',{'opsCadastradas': opsCadastradas,'opsIniciadas': opsIniciadas})
+    return render(request, 'home/index.html',{'opsCadastradas': opsCadastradas,'opsIniciadas': opsIniciadas,'op_aberta': op_aberta})
